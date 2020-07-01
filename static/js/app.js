8b6010f2820c2616d84f2d0f8dc75359c577e8db
Sami Elkhayri <selkhayri@gmail.com>
2020-07-01 13:25:51 -0400

    Used D3 instead of DOM

--- static/js/app.js
+++ static/js/app.js
@@ -24,233 +24,216 @@ function buildTable(data) {
     });
   }
 
-  function handleClick() {
-    if (allUnchecked()) {
-      alert("Please specify filter values!");
-      return;
-    }
+// function handleClick() {
+function filterTable() {
+ 
+  // Grab the filter values
+  if (d3.select("#chkDate").property("checked")) {
+    var date = d3.select("#filterDate"); 
+  }
 
-    // Grab the filter values
-    if (document.getElementById("filterDate")) {
-      var date = d3.select("#filterDate"); 
-    }
+  if (d3.select("#chkCity").property("checked")) {
+    var city = d3.select("#filterCity"); 
+  }
 
-    if (document.getElementById("filterCity")) {
-      var city = d3.select("#filterCity"); 
-    }
+  if (d3.select("#chkState").property("checked")) {
+    var state = d3.select("#filterState"); 
+  }
 
-    if (document.getElementById("filterState")) {
-      var state = d3.select("#filterState"); 
-    }
+  if (d3.select("#chkCountry").property("checked")) {
+    var country = d3.select("#filterCountry"); 
+  }
 
-    if (document.getElementById("filterCountry")) {
-      var country = d3.select("#filterCountry"); 
-    }
+  if (d3.select("#chkShape").property("checked")) {
+    var shape = d3.select("#filterShape"); 
+  }
 
-    if (document.getElementById("filterShape")) {
-      var shape = d3.select("#filterShape"); 
-    }
-
-    let filteredData = tableData;
-    let missing = "Missing Values:\n";
-    let values = "";
-     // Check to see if a date was entered and filter the
-    // data using that date.
-    if (date) {
-      console.log(date);
-      let dateValue = date.property("value");
-      // Apply `filter` to the table data to only keep the
-      // rows where the `datetime` value matches the filter value
-      if (dateValue != "") {
-        filteredData = filteredData.filter(row => row.datetime === dateValue);
-      }
-      else {
-        values += "Filter Date\n";
-      }
-    }
-    if (city) {
-      let cityValue = city.property("value");
-      if (cityValue != "") {
-        filteredData = filteredData.filter((row) => row.city === cityValue);
-      }
-      else {
-        values += "Filter City\n";
-      }
-    }
-    if (state) {
-      let stateValue = state.property("value");
-      if (stateValue != "") {
-        filteredData = filteredData.filter((row) => row.state === stateValue);
-      }
-      else {
-        values += "Filter State\n"
-      }
-    }
-    if (country) {
-      let countryValue = country.property("value");
-      if (countryValue != "") {
-        filteredData = filteredData.filter((row) => row.country === countryValue);
-      }
-      else {
-        values += "Filter Country\n";
-      }
-    }
-    if(shape) {
-      shapeValue = shape.property("value");
-      if (shapeValue != "") {
-        filteredData = filteredData.filter((row) => row.shape === shapeValue);
-      }
-      else {
-        values += "Filter Shape\n";
-      }
-    }
-    
-    if (values != "") {
-      alert(missing + "\n" + values);
+  let filteredData = tableData;
+  let missing = "Missing Values:\n";
+  let values = "";
+    // Check to see if a date was entered and filter the
+  // data using that date.
+  if (date) {
+    console.log(date);
+    let dateValue = date.property("value");
+    // Apply `filter` to the table data to only keep the
+    // rows where the `datetime` value matches the filter value
+    if (dateValue != "") {
+      filteredData = filteredData.filter(row => row.datetime === dateValue);
     }
     else {
-     // Rebuild the table using the filtered data
-    // @NOTE: If no date was entered, then filteredData will
-    // just be the original tableData.
-      buildTable(filteredData);
+      values += "Filter Date\n";
     }
   }
-
-  function addFilter(field) {
-    var btnID = "";
-    var forfield = "";
-    var placeholder = "";
-    var labelInnerHTML = "";
-    var listItemId = "";
-
-    listItemId = "filterBy" + field;
-    btnID = "btnFilterBy" + field;
-    forfield = field;
-    labelInnerHTML = "Enter " + field;
-    inputId = "filter" + field;
-
-    if ( field == "Date") {
-      placeholder = "1/10/2010";
+  if (city) {
+    let cityValue = city.property("value");
+    if (cityValue != "") {
+      filteredData = filteredData.filter((row) => row.city === cityValue);
     }
-    else if (field == "City") {
-      placeholder = "Toronto";
+    else {
+      values += "Filter City\n";
     }
-    else if (field == "State") {
-      placeholder = "Ontario";
-    }
-    else if (field == "Country") {
-      placeholder = "Canada";
-    }
-    else if (field == "Shape") {
-      placeholder = "circle";
-    }
-
-    filterList = document.getElementById("filters");
-    
-    filterbtn = document.createElement("button");
-    filterbtn.setAttribute("id",btnID);
-    filterbtn.setAttribute("type","button");
-    filterbtn.setAttribute("class","btn btn-dark");
-
-    // g2 = document.createElement("li");
-    // g2.setAttribute("class","bg-dark list-group-item");
-    // g2.appendChild(filterbtn);
-    
-    label = document.createElement("label");
-    label.setAttribute("for",forfield);
-    label.innerHTML = labelInnerHTML;
-    input = document.createElement("input"); 
-    input.setAttribute("type","text");
-    input.setAttribute("placeholder",placeholder);
-    input.setAttribute("id",inputId);
-    
-    g1 = document.createElement("li");
-    g1.setAttribute("class","bg-dark list-group-item");
-    g1.appendChild(label);
-    g1.append(document.createElement("br"));
-    g1.appendChild(input);
-    
-    ul = document.createElement("ul"); 
-    ul.appendChild(g1);
-    // ul.appendChild(g2);
-
-    li = document.createElement("li"); 
-    li.setAttribute("id",listItemId);
-    li.setAttribute("class","bg-dark list-list-group-item")
-    li.appendChild(ul);
-    
-    filterList.appendChild(li);
-    
   }
-
-  function allUnchecked() {
-    chkboxes = getCheckboxes();
-    for (var i=0;i<chkboxes.length;i++){
-      if ( chkboxes[i].checked ) {
-        return false;
-      }
+  if (state) {
+    let stateValue = state.property("value");
+    if (stateValue != "") {
+      filteredData = filteredData.filter((row) => row.state === stateValue);
     }
-    return true;
+    else {
+      values += "Filter State\n"
+    }
   }
-
-  function handleCheck() {
-    var filterName=null;
-
-    src = event.target;
+  if (country) {
+    let countryValue = country.property("value");
+    if (countryValue != "") {
+      filteredData = filteredData.filter((row) => row.country === countryValue);
+    }
+    else {
+      values += "Filter Country\n";
+    }
+  }
+  if(shape) {
+    shapeValue = shape.property("value");
+    if (shapeValue != "") {
+      filteredData = filteredData.filter((row) => row.shape === shapeValue);
+    }
+    else {
+      values += "Filter Shape\n";
+    }
+  }
   
-    switch(src.id) {
-      case "chkDate": filterId = "filterByDate"; break;
-      case "chkCity": filterId = "filterByCity"; break;
-      case "chkState": filterId = "filterByState"; break;
-      case "chkCountry": filterId = "filterByCountry"; break;
-      case "chkShape": filterId = "filterByShape"; break;
-    }
+  if (values != "") {
+    alert(missing + "\n" + values);
+  }
+  else {
+    // Rebuild the table using the filtered data
+  // @NOTE: If no date was entered, then filteredData will
+  // just be the original tableData.
+    buildTable(filteredData);
+  }
+}
 
-    filter = document.getElementById(filterId);
-    if (filter) {
-      filters.removeChild(filter);
+function addFilter(field) {
+  var btnID = "";
+  var forfield = "";
+  var placeholder = "";
+  var labelInnerHTML = "";
+  var listItemId = "";
+
+  listItemId = "filterBy" + field;
+  btnID = "btnFilterBy" + field;
+  forfield = field;
+  labelInnerHTML = "Enter " + field;
+  inputId = "filter" + field;
+
+  if ( field == "Date") {
+    placeholder = "1/10/2010";
+  }
+  else if (field == "City") {
+    placeholder = "Toronto";
+  }
+  else if (field == "State") {
+    placeholder = "Ontario";
+  }
+  else if (field == "Country") {
+    placeholder = "Canada";
+  }
+  else if (field == "Shape") {
+    placeholder = "circle";
+  }
+
+  d3.select("#filters").append("li").attr("id","g1"+listItemId)
+                          .attr("class","bg-dark list-group-item");
+  
+  d3.select("#g1"+listItemId).append("label")
+                              .attr("for",forfield)
+                              .html(labelInnerHTML);
+  
+  d3.select("#g1"+listItemId).append("br");                         
+  d3.select("#g1"+listItemId).append("input")
+                              .attr("type","text")
+                              .attr("id",inputId)
+                              .attr("placeholder",placeholder)
+                              .attr("class","btn btn-dark");
+}
+
+function allUnchecked() {
+  chkboxes = getCheckboxes();
+  for (var i=0;i<chkboxes.length;i++){
+    if ( chkboxes[i].checked )  {
+      return false;
     }
-    else {
-      if (filterId == "filterByDate") {
+  }
+  return true;
+}
+
+function handleCheck() {
+  var filterName=null;
+
+  src = event.target;
+
+  switch(src.id) {
+    case "chkDate": filterId = "filterByDate"; break;
+    case "chkCity": filterId = "filterByCity"; break;
+    case "chkState": filterId = "filterByState"; break;
+    case "chkCountry": filterId = "filterByCountry"; break;
+    case "chkShape": filterId = "filterByShape"; break;
+  }
+
+  if (!src.checked) {
+    console.log("Removing filter");
+    d3.select("#filters").select("#g1" + filterId).remove();
+  }
+  else {
+    if (filterId === "filterByDate") {
+      g1filterByDate = d3.select("#g1filterByDate");
+      console.log("g1filterByDate.id = " + g1filterByDate["id"]);
+      if (g1filterByDate.id) {
+        d3.select("#filters").append(g1filterByDate);
+      }
+      else {
         addFilter("Date");
       }
-      else if (filterId == "filterByCity") {
-        addFilter("City");
-      }
-      else if (filterId == "filterByState") {
-        addFilter("State");
-      }
-      else if (filterId == "filterByCountry") {
-        addFilter("Country");
-      }
-      else if (filterId == "filterByShape"){
-        addFilter("Shape");
-      }
+    }
+    else if (filterId === "filterByCity") {
+      addFilter("City");
+    }
+    else if (filterId === "filterByState") {
+      addFilter("State");
+    }
+    else if (filterId === "filterByCountry") {
+      addFilter("Country");
+    }
+    else if (filterId === "filterByShape"){
+      addFilter("Shape");
     }
   }
+}
 
-  function getCheckboxes() {
-    var checkboxes = []
-    elements = document.getElementsByTagName("input")
-    for(var i=0;i<elements.length;i++) {
-      id = String(elements[i].id);
-      
-      if (elements[i].id.startsWith("chk")) {
-         checkboxes.push(elements[i])
-      }
-    }  
-    return checkboxes;
-  }
+function getCheckboxes() {
+  var checkboxes = []
+  // elements = document.getElementsByTagName("input")
+  elements = d3.selectAll(".fil-box").nodes();
 
-  function resetForm() {
-    chkboxes = getCheckboxes();
-    chkboxes.forEach((chk) => chk.checked = false);
+  for(var i=0;i<elements.length;i++) {
+   //  if (elements[i].property("id").startsWith("chk")) {
+        checkboxes.push(elements[i])
+   // }
+  }  
+  return checkboxes;
+}
 
-    filters = document.getElementById("filters");
-    filters.innerHTML = "";
+function resetForm() {
+  chkboxes = getCheckboxes();
+  chkboxes.forEach((chk) => chk.checked = false);
 
-    buildTable(tableData);
-  }
+  // filters = document.getElementById("filters");
+  // filters.innerHTML = "";
+  d3.select("#filters").html("");
+
+  buildTable(tableData);
+}
+
 // Attach an event to listen for the form button
 checkboxes = getCheckboxes();
 checkboxes.forEach((cb) => d3.selectAll("#"+cb.id).on("click", handleCheck));
@@ -261,7 +244,8 @@ buildTable(tableData);
 checkboxes.forEach((cb) => cb.checked = false);
 
 filterBtn = document.getElementById("filter-btn");
-d3.selectAll("#filter-btn").on("click", handleClick);
+//d3.selectAll("#filter-btn").on("click", handleClick);
+d3.selectAll("#filter-btn").on("click", filterTable);
 
 resetBtn = document.getElementById("reset-btn");
 d3.selectAll("#reset-btn").on("click", resetForm);
